b0a356ef53df5c5c727259559b86461d7118cef2
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-03 10:18:09 -0300

    Actualizar utils.py

--- utils.py
+++ utils.py
@@ -1,36 +1,14 @@
 import datetime
 import json
+import os
 
-QUEUE_FILE = "queue.json"
 LOG_FILE = "logs.txt"
 
 def log(text):
-    with open(LOG_FILE, "a") as f:
-        f.write(f"[{datetime.datetime.now()}] {text}\n")
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(LOG_FILE, "a", encoding="utf-8") as f:
+        f.write(f"[{timestamp}] {text}\n")
 
-
-def add_to_queue(url):
-    try:
-        with open(QUEUE_FILE, "r") as f:
-            data = json.load(f)
-    except:
-        data = []
-
-    data.append(url)
-
-    with open(QUEUE_FILE, "w") as f:
-        json.dump(data, f)
-
-
-def remove_from_queue(url):
-    try:
-        with open(QUEUE_FILE, "r") as f:
-            data = json.load(f)
-    except:
-        return
-
-    if url in data:
-        data.remove(url)
-
-    with open(QUEUE_FILE, "w") as f:
-        json.dump(data, f)
+def clean_file(filepath):
+    if os.path.exists(filepath):
+        os.remove(filepath)
