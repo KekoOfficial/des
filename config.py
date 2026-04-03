d145f5b8a44e6c68c36199c908e0fbc3ea3378bf
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-03 10:22:29 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-TOKEN = "TU_TOKEN_DE_TELEGRAM"
-DOWNLOAD_PATH = "downloads"
-# Velocidad de fragmentos (hilos en paralelo)
-CONCURRENT_FRAGMENTS = 15
+TOKEN = "TU_TOKEN"
+CONCURRENT_FRAGMENTS = 15 # Velocidad de fragmentos solicitada
+MAX_CONCURRENT_DOWNLOADS = 1 # Procesar 1 video a la vez para no saturar CPU/Banda
